acdebc66b057bda54d1403ab0bb929ba3f6f46c5
lijiaqi <lijq1103@163.com>
2024-02-17 14:57:10 +0800

    feat: project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,9 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '点对点的视频通话',
+    description: `这是一个使用了 WebRTC 技术的视频通话app，它是一个免费、点对点、开源的网络视频通话应用。`,
+    imgSrc: '/static/images/project-talk.png',
+    href: 'https://talk.justdev.cn/',
   },
 ]
 
